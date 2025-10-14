0bb5e3b8cb105ad3020384fc7ef941d37a3f32d1
John Watson <john.watson1@defra.gov.uk>
2025-10-14 21:50:00 +0100

    Update config

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,8 +1,4 @@
 import { health } from '../routes/health.js'
-import { payments } from '../routes/payments.js'
-import { paymentsSearch } from '../routes/payments-search.js'
-import { paymentsSummary } from '../routes/payments-summary.js'
-import { paymentsPayee } from '../routes/payments-payee.js'
 
 const router = {
   plugin: {
@@ -10,11 +6,7 @@ const router = {
     register: (server, _options) => {
       server.route(
         [].concat(
-          health,
-          payments,
-          paymentsSearch,
-          paymentsSummary,
-          paymentsPayee
+          health
         )
       )
     }
