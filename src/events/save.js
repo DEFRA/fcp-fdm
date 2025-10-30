659229be318ee8ae3f5995f880537a0d17f805de
John Watson <john.watson1@defra.gov.uk>
2025-10-30 12:48:16 +0000

    Remove dynamic imports

--- src/events/save.js
+++ src/events/save.js
@@ -1,4 +1,10 @@
+import { save as messageSaver } from './save/message.js'
+
+const savers = {
+  message: messageSaver
+}
+
 export async function saveEvent (event, eventType) {
-  const { save } = await import(`./save/${eventType}.js`)
+  const save = savers[eventType]
   await save(event)
 }
--- src/events/validate.js
+++ src/events/validate.js
@@ -1,7 +1,13 @@
-export async function validateEvent (event, eventType) {
-  const schema = await import(`./schemas/${eventType}.js`)
+import messageSchema from './schemas/message.js'
 
-  const validationResult = schema.default.validate(event, { abortEarly: false, allowUnknown: true })
+const validators = {
+  message: messageSchema
+}
+
+export async function validateEvent (event, eventType) {
+  const schema = validators[eventType]
+
+  const validationResult = schema.validate(event, { abortEarly: false, allowUnknown: true })
 
   if (validationResult.error) {
     throw new Error(`Event is invalid, ${validationResult.error.message}`)
