11eda98f08e5bcb200e2be805872f4601bd7c4a6
John Watson <john.watson1@defra.gov.uk>
2025-10-30 12:19:25 +0000

    Persist only summary

--- src/events/save/message.js
+++ src/events/save/message.js
@@ -28,6 +28,16 @@ export async function save (event) {
     }
   }
 
+  const eventSummary = {
+    _id: eventEntity._id,
+    type: eventEntity.type,
+    source: eventEntity.source,
+    id: eventEntity.id,
+    time: eventEntity.time,
+    subject: eventEntity.subject,
+    received: eventEntity.received
+  }
+
   // save new/update existing message
   await db.collection(messageCollection).updateOne(
     { _id: correlationId },
@@ -60,7 +70,7 @@ export async function save (event) {
                       cond: { $ne: ['$$e._id', eventEntity._id] }
                     }
                   },
-                  [eventEntity]
+                  [eventSummary]
                 ]
               }
             }
