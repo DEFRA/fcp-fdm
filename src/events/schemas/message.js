d9a8251af1d3a1a52c11ffc4e067cc3e5d2bf4b0
John Watson <john.watson1@defra.gov.uk>
2025-10-31 14:21:47 +0000

    Add memory monitor

--- src/events/schemas/message.js
+++ src/events/schemas/message.js
@@ -1,9 +1,11 @@
 import Joi from 'joi'
 import cloudEvent from './cloud-event.js'
 
-export default cloudEvent.keys({
+const message = cloudEvent.keys({
   data: Joi.object({
     correlationId: Joi.string().required(),
     recipient: Joi.string().required(),
   }).required()
 })
+
+export default message
