533c548a59d257db761c57e7359e632999b0f0ea
John Watson <john.watson1@defra.gov.uk>
2026-01-22 15:11:46 +0000

    Add logging

--- src/events/process.js
+++ src/events/process.js
@@ -1,11 +1,15 @@
+import { createLogger } from '../common/helpers/logging/logger.js'
 import { parseEvent } from './parse.js'
 import { getEventType } from './types.js'
 import { validateEvent } from './validate.js'
 import { saveEvent } from './save.js'
+
+const logger = createLogger()
 
 export async function processEvent (rawEvent) {
   const event = parseEvent(rawEvent)
   const eventType = getEventType(event.type)
   await validateEvent(event, eventType)
   await saveEvent(event, eventType)
+  logger.info({ id: event.id, type: event.type }, 'Event processed successfully')
 }
